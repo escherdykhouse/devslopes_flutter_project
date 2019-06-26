c452e7a46d9211347946c04c2c0de040cc6c27c7
escherdykhouse <escherwd@icloud.com>
2019-06-26 10:10:00 -0700

    Android Native
    you can now get the ip address on android, next step: ios.

--- devslopes_course_app/android/app/src/main/java/com/whitmanpartners/devslopes_course_app/MainActivity.java
+++ devslopes_course_app/android/app/src/main/java/com/whitmanpartners/devslopes_course_app/MainActivity.java
@@ -1,13 +1,70 @@
 package com.whitmanpartners.devslopes_course_app;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+
 public class MainActivity extends FlutterActivity {
+
+  public String getLocalIpAddress(){
+    try {
+      for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+           en.hasMoreElements();) {
+        NetworkInterface intf = en.nextElement();
+        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+          InetAddress inetAddress = enumIpAddr.nextElement();
+          if (!inetAddress.isLoopbackAddress()) {
+            return inetAddress.getHostAddress();
+          }
+        }
+      }
+    } catch (Exception ex) {
+      Log.e("IP Address", ex.toString());
+    }
+    return null;
+  }
+
+  private static final String CHANNEL = "pupbook.native/";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+            new MethodCallHandler() {
+              @java.lang.Override
+              public void onMethodCall(MethodCall methodCall, Result result) {
+                if (methodCall.method.equals("getIpAddress")) {
+                  String ip = getIpAddress();
+                  if (ip != null) {
+                    result.success(ip);
+                  } else {
+                    result.error("Couldn't get IP",null,null);
+                  }
+                } else {
+                  result.notImplemented();
+                }
+              }
+            }
+    );
+  }
+
+  private String getIpAddress() {
+    String IPAddress = getLocalIpAddress();
+
+    return IPAddress;
   }
 }
--- devslopes_course_app/lib/radcode.dart
+++ devslopes_course_app/lib/radcode.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import 'posts_list.dart';
 
+import 'dart:async';
+import 'package:flutter/services.dart';
+
 class RadCodePage extends StatefulWidget {
   @override
   _RadCodePageState createState() => _RadCodePageState();
 }
 
 class _RadCodePageState extends State<RadCodePage> {
+
+  String _ipAddress = 'unknown';
+
+  Future<void> _getIpAddress() async {
+    String ipAddress;
+
+  try {
+    const platform = const MethodChannel('pupbook.native/');
+    final String result = await platform.invokeMethod('getIpAddress');
+    ipAddress = "Your IP Address is: $result";
+  } on PlatformException catch (e) {
+    ipAddress = "A native issue occurred\n${e.message}";
+  } catch (e) {
+    ipAddress = "A flutter issue occurred\n${e.message}";
+  }
+
+    setState(() {
+     _ipAddress = ipAddress; 
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +43,17 @@ class _RadCodePageState extends State<RadCodePage> {
         child: Container(
           padding: EdgeInsets.only(left: 20, top: 44),
           color: Colors.white,
-          child: Text("Test"),
+          child: Column(
+            children: <Widget>[
+              Text(_ipAddress),
+              FlatButton(
+                child: Icon(Icons.refresh),
+                onPressed: () {
+                  _getIpAddress();
+                },
+              )
+            ],
+          )
         ),
       ),
     );
